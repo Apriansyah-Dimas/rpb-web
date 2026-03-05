d8068079d85449d0b9ef00ac6e6133e129d9f7bd
Apriansyah-Dimas <muhmmddimas13@gmail.com>
2026-03-05 10:55:08 +0700

    fd

--- src/components/admin/admin-config-panel.tsx
+++ src/components/admin/admin-config-panel.tsx
@@ -1020,10 +1020,10 @@ export function AdminConfigPanel() {
                 <thead className="bg-[#f6f7ff]">
                   <tr className="text-left text-xs font-semibold text-rpb-ink-soft">
                     <th className="w-[14%] px-3 py-2">Category</th>
-                    <th className="w-[20%] px-3 py-2">Name</th>
+                    <th className="w-[25%] px-3 py-2">Name</th>
                     <th className="w-[20%] px-3 py-2">Model</th>
                     <th className="w-[10%] px-3 py-2">Unit</th>
-                    <th className="w-[20%] px-3 py-2">Harga</th>
+                    <th className="w-[15%] px-3 py-2">Harga</th>
                     <th className="w-[16%] px-3 py-2">Aksi</th>
                   </tr>
                 </thead>
